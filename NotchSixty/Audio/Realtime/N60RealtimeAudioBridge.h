db6cfe2d47afc817b4d12783075f44546fa0a6af
DHDook <dhasandras@icloud.com>
2026-09-20 22:16:28 -0400

    Add realtime output startup gate

--- NotchSixty/Audio/Realtime/N60RealtimeAudioBridge.h
+++ NotchSixty/Audio/Realtime/N60RealtimeAudioBridge.h
@@ -2,6 +2,7 @@
 #define N60RealtimeAudioBridge_h
 
 #include <CoreAudio/CoreAudio.h>
+#include <stdbool.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
@@ -18,13 +19,17 @@ typedef struct {
     uint64_t underrunFrames;
     uint64_t overrunFrames;
     uint64_t unsupportedBufferLayouts;
+    uint64_t gatedOutputCallbacks;
+    uint64_t gatedOutputFrames;
     uint32_t bufferedFrames;
+    bool outputGateOpen;
 } N60RealtimeAudioBridgeSnapshot;
 
 N60RealtimeAudioBridge * _Nullable N60RealtimeAudioBridgeCreate(uint32_t capacityFrames);
 void N60RealtimeAudioBridgeDestroy(N60RealtimeAudioBridge * _Nonnull bridge);
 void N60RealtimeAudioBridgeReset(N60RealtimeAudioBridge * _Nonnull bridge);
 void N60RealtimeAudioBridgeSetOutputGain(N60RealtimeAudioBridge * _Nonnull bridge, float gain);
+void N60RealtimeAudioBridgeConfigureOutputGate(N60RealtimeAudioBridge * _Nonnull bridge, uint32_t minimumBufferedFrames);
 N60RealtimeAudioBridgeSnapshot N60RealtimeAudioBridgeGetSnapshot(const N60RealtimeAudioBridge * _Nonnull bridge);
 
 OSStatus N60CaptureIOProc(
