f54cab2e9674d0a2610ebc8271c69aaab18b3b3b
DHDook <dhasandras@icloud.com>
2026-09-25 15:50:52 -0400

    Expand PR28 dynamics conditioning API

--- NotchSixty/Audio/Realtime/N60Dynamics.h
+++ NotchSixty/Audio/Realtime/N60Dynamics.h
@@ -12,6 +12,33 @@ extern "C" {
 #endif
 
 #define N60_MULTIBAND_BAND_COUNT 3
+#define N60_MAX_INFRASONIC_SECTIONS 8
+
+typedef enum {
+    N60InfrasonicSlope24DBPerOctave = 0,
+    N60InfrasonicSlope48DBPerOctave = 1,
+    N60InfrasonicSlope96DBPerOctave = 2,
+} N60InfrasonicSlope;
+
+typedef struct {
+    bool enabled;
+    float poleCoefficient;
+} N60DCOffsetFilterSnapshot;
+
+typedef struct {
+    bool enabled;
+    double cutoffHz;
+    N60InfrasonicSlope slope;
+    uint32_t sectionCount;
+    N60BiquadCoefficients highPass[N60_MAX_INFRASONIC_SECTIONS];
+} N60InfrasonicFilterSnapshot;
+
+typedef struct {
+    bool enabled;
+    float strength;
+    N60BiquadCoefficients lowShelf;
+    N60BiquadCoefficients highShelf;
+} N60LoudnessContourSnapshot;
 
 typedef struct {
     bool enabled;
@@ -71,6 +98,9 @@ typedef struct {
 } N60PauseGateSnapshot;
 
 typedef struct {
+    N60DCOffsetFilterSnapshot dcOffsetFilter;
+    N60InfrasonicFilterSnapshot infrasonicFilter;
+    N60LoudnessContourSnapshot loudnessContour;
     N60DeEsserSnapshot deEsser;
     N60MultibandCompressorSnapshot multibandCompressor;
     N60CompressorSnapshot compressor;
@@ -80,6 +110,19 @@ typedef struct {
 } N60DynamicsSnapshot;
 
 typedef struct {
+    float dcPreviousInputLeft;
+    float dcPreviousInputRight;
+    float dcPreviousOutputLeft;
+    float dcPreviousOutputRight;
+    float dcMix;
+    N60BiquadState infrasonicLeft[N60_MAX_INFRASONIC_SECTIONS];
+    N60BiquadState infrasonicRight[N60_MAX_INFRASONIC_SECTIONS];
+    float infrasonicMix;
+    N60BiquadState loudnessLowShelfLeft;
+    N60BiquadState loudnessLowShelfRight;
+    N60BiquadState loudnessHighShelfLeft;
+    N60BiquadState loudnessHighShelfRight;
+    float loudnessMix;
     float deEsserGainDB;
     N60BiquadState deEsserHighPassLeft;
     N60BiquadState deEsserHighPassRight;
@@ -110,6 +153,27 @@ typedef struct {
 } N60DynamicsTelemetry;
 
 N60DynamicsSnapshot N60DynamicsSnapshotMakeBypassed(double sampleRate);
+
+bool N60DynamicsSnapshotSetDCOffsetFilter(
+    N60DynamicsSnapshot * _Nonnull snapshot,
+    double sampleRate,
+    bool enabled
+);
+
+bool N60DynamicsSnapshotSetInfrasonicFilter(
+    N60DynamicsSnapshot * _Nonnull snapshot,
+    double sampleRate,
+    bool enabled,
+    double cutoffHz,
+    N60InfrasonicSlope slope
+);
+
+bool N60DynamicsSnapshotSetLoudnessContour(
+    N60DynamicsSnapshot * _Nonnull snapshot,
+    double sampleRate,
+    bool enabled,
+    float strength
+);
 
 bool N60DynamicsSnapshotSetDeEsser(
     N60DynamicsSnapshot * _Nonnull snapshot,
@@ -168,6 +232,12 @@ bool N60DynamicsSnapshotSetPauseGate(
 
 bool N60DynamicsSnapshotIsValid(N60DynamicsSnapshot snapshot);
 void N60DynamicsRuntimeReset(N60DynamicsRuntime * _Nonnull runtime);
+void N60DynamicsProcessPreEQStereoFrame(
+    N60DynamicsRuntime * _Nonnull runtime,
+    N60DynamicsSnapshot snapshot,
+    float * _Nonnull left,
+    float * _Nonnull right
+);
 void N60DynamicsProcessCoreStereoFrame(
     N60DynamicsRuntime * _Nonnull runtime,
     N60DynamicsSnapshot snapshot,
